c8e90d5a47d7f3581f36c1eaca5b230c514166c1
Arsenii Gorushkin <arsenii.gorushkin@gmail.com>
2023-01-10 15:01:40 +1300

    refactor: moved bounty system to a different branch

--- core/auth/fetchUserData.ts
+++
@@ -1,4 +0,0 @@
-export const fetchUserData = async <T = unknown>(endpoint: string, token: string): Promise<T> => {
-  const response = await fetch(`https://discord.com/api/${ endpoint }`, { headers: { authorization: `Bearer ${ token }` } });
-  return await response.json();
-};
--- main.ts
+++ main.ts
@@ -1,10 +1,1 @@
 import '/client/main.ts';
-import '/server/main.ts';
-
-import { initializeApp } from 'firebase/app';
-import { getFirestore, } from 'firestore';
-
-import { FirebaseConfig } from '/config.ts';
-
-const firebase  = initializeApp(FirebaseConfig);
-export const db = getFirestore(firebase);
