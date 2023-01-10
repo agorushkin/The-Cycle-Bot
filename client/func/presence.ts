047530b569992f707a7e3617f92d11640d9fb774
Arsenii Gorushkin <arsenii.gorushkin@gmail.com>
2023-01-10 15:31:32 +1300

    fix: changed to only one line displayed

--- client/func/presence.ts
+++ client/func/presence.ts
@@ -3,33 +3,21 @@ import { Client } from 'harmony';
 import { fetchServerInfo } from '/core/dayz/fetchServerInfo.ts';
 import { dayZFormatter } from '/core/dayz/dayZFormatter.ts';
 
-const PRESENCE = [ [ 'PLAYER_COUNT', 'TIME', 'ADDRESS' ], 0 ] as [ string[], number ];
-
 export const startClientPresenceUpdate = (client: Client) => {
   setInterval(async () => {
     const data = await fetchServerInfo();
 
     if (data === null) return;
 
-    const { status, environment, host } = data;
+    const { status } = data;
 
     const playerFormat  = dayZFormatter.players(status);
-    const timeFormat    = dayZFormatter.time(environment, true);
-    const addressFormat = dayZFormatter.address(host, 'game');
-
-    const activities = [
-      `Players - ${ playerFormat }`,
-      `Time - ${ timeFormat }`,
-      `Address - ${ addressFormat }`,
-    ];
 
     client.setPresence({
       activity: {
-        name: activities[PRESENCE[1]],
+        name: `Players - ${ playerFormat }`,
         type: 'WATCHING',
       },
     });
-
-    PRESENCE[1] === PRESENCE[0].length - 1 ? PRESENCE[1] = 0 : PRESENCE[1]++;
   }, 5000);
 }
